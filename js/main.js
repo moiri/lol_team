94e4eedeb16773239dce1cb76520eb0975bc74c4
Simon Maurer <moirelein@gmail.com>
2015-05-24 02:07:48 +0100

    fix

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-    var url, teams = [];
-    url = 'data/teams.json';
+    var url ;
+    url = 'data/team.json';
     $.getJSON(url, function(json) {
-        for (id in json) teams.push(id);
-        $.each(json[teams[0]].matchHistory, function (idx, match) {
+        $.each(json.matchHistory, function (idx, match) {
             var css = 'loss';
             if (match.win) css = 'win';
             $('.leftColumn').append(
