b2d770ebfc8cb01b604a4beeb5347266f17df819
Simon Maurer <moirelein@gmail.com>
2015-06-01 21:36:58 +0100

    get summonersId from team

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-    var url, data;
-    data = {};
+    var data = {};
     data.summonerId = null;
     data.opposingTeam = '0';
     data.lane = null;
-    url = 'data/team.json';
-    $.getJSON(url, function(team) {
+    $.getJSON('data/team.json', function(team) {
         $('.header').html(
             team.name + " " + team.teamStatDetails[0].wins + ":"
             + team.teamStatDetails[0].losses
@@ -14,46 +12,46 @@ $(document).ready(function() {
                          + team.teamStatDetails[0].losses)).toFixed(2)
             + ")"
         );
-    });
-    url = 'data/summoners.json';
-    $.getJSON(url, function(summoners) {
-        $.each(summoners, function (id, name) {
+        $.getJSON('data/summoners.json', function(summoners) {
+            $.each(team.roster.memberList, function (idx, member) {
+                $('.leftColumn').append(
+                    '<div id="summoner-' + member.playerId
+                    + '" class="summoner">' + summoners[member.playerId] + '</div>'
+                );
+            });
             $('.leftColumn').append(
-                '<div id="summoner-' + id + '" class="summoner">' + name + '</div>'
+                '<div id="summoner-opposingTeam" class="summoner opposingTeam">Opposing Team</div>'
             );
-        });
-        $('.leftColumn').append(
-            '<div id="summoner-opposingTeam" class="summoner opposingTeam">Opposing Team</div>'
-        );
-        $('.leftColumn').append(
-            '<div id="clear" class="clear">clear</div>'
-        );
-        $('.summoner').click(function() {
-            var id;
-            data.opposingTeam = '0';
-            data.summonerId = null;
-            if (!$(this).hasClass('active')) {
+            $('.leftColumn').append(
+                '<div id="clear" class="clear">clear</div>'
+            );
+            $('.summoner').click(function() {
+                var id;
+                data.opposingTeam = '0';
+                data.summonerId = null;
+                if (!$(this).hasClass('active')) {
+                    $('.summoner').removeClass('active');
+                    $(this).addClass('active');
+                    id = $(this).attr('id').split('-');
+                    if (id[1] === 'opposingTeam')
+                        data.opposingTeam = '1'
+                    else
+                        data.summonerId = id[1];
+                }
+                else {
+                    $(this).removeClass('active');
+                }
+                getWinrate(data);
+            });
+            $('.clear').click(function() {
+                data = {};
                 $('.summoner').removeClass('active');
-                $(this).addClass('active');
-                id = $(this).attr('id').split('-');
-                if (id[1] === 'opposingTeam')
-                    data.opposingTeam = '1'
-                else
-                    data.summonerId = id[1];
-            }
-            else {
-                $(this).removeClass('active');
-            }
-            getWinrate(data);
-        });
-        $('.clear').click(function() {
-            data = {};
-            $('.summoner').removeClass('active');
-            $('.filter').removeClass('active');
+                $('.filter').removeClass('active');
+                getWinrate();
+            });
             getWinrate();
         });
     });
-    getWinrate();
 });
 
 function getWinrate(data) {
